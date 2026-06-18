3f8456d2a6b457ac706259083e4b5335fb1c0fdc
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2026-06-18 17:12:36 +0100

    add models Categorie, Team, Athlete, Comment

---
+++ database/factories/AthleteFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Athlete;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Athlete>
+ */
+class AthleteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
